e2f1335bd41495d47f126d07491afc0f111da7de
kihamaster <121562098+kihamaster@users.noreply.github.com>
2023-01-01 21:39:35 +0900

    Add files via upload

---
+++ view2.php
@@ -0,0 +1,36 @@
+<?php
+    include "lib.php";
+
+    $idx = $_GET['idx'];
+    $idx = mysqli_real_escape_string($connect, $idx);
+    
+    $query = "select * from board2 where idx= '$idx' ";
+    $result = mysqli_query($connect, $query);
+    $data = mysqli_fetch_array($result);
+
+    
+
+?>
+<form action="writepost2.php" method="post">
+    <table width=800 border="1" cellpadding=10 >
+    <tr>
+        <th width=100>제목 </th>
+        <td> <?=$data['subject']?></td>
+       </tr>
+    <tr>
+        <th width=100>작성자 </th>
+        <td> <?=$data['name']?> </td>
+       </tr>
+    <tr>
+        <th width=100>소개 </th>
+        <td> <?=nl2br($data['memo'])?>
+        </td>
+       </tr>
+       
+       <tr>
+            <td colspan="3">    
+            <a href="list2.php">목록</a>
+            </td>
+       </tr>
+    </table>
+</form>
---
+++ writepost2.php
@@ -0,0 +1,24 @@
+<?php
+    include "lib.php";
+
+    $name = $_POST['name'];
+   
+    $subject = $_POST['subject'];
+    $memo = $_POST['memo'];
+
+    $name = mysqli_real_escape_string($connect, $name);
+    $subject = mysqli_real_escape_string($connect, $subject);
+    $memo = mysqli_real_escape_string($connect, $memo);
+    
+    $regdate = date("Y-m-d H:i:s");
+     
+    $query = "insert into board2(name, subject, memo, regdate)
+        values('$name','$subject', '$memo', '$regdate') ";
+
+    mysqli_query($connect, $query);
+
+?>
+<script>
+    location.href='list2.php';
+</script>
+
