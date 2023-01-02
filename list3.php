d69fd80b75cf49dd3ad573ef7d5acb14ffd507f3
kihamaster <121562098+kihamaster@users.noreply.github.com>
2023-01-02 23:32:34 +0900

    Add files via upload

---
+++ list3.php
@@ -0,0 +1,29 @@
+<?php
+    include "lib.php";
+
+?>
+
+<table width=800 border="1">
+    <tr>
+        <th width=40 >No </th>
+        <th>제목 </th>
+        <th width=90>작성자 </th>
+        <th>작성시간 </th>
+    </tr>
+<?php
+    
+    $query = "select * from board3 order by idx desc";
+    $result = mysqli_query($connect, $query);
+
+    while($data = mysqli_fetch_array($result)){
+?>
+    <tr>
+        <td> <?=$data['idx']?> </td>
+        <td> <a href="view3.php?idx=<?=$data['idx']?>"><?=$data['subject']?></a> </td>
+        <td> <?=$data['name']?> </td>
+        <td> <?=$data['regdate']?> </td>
+
+<?php } ?>
+</table>
+
+<a href="write3.php">글쓰기</a>
